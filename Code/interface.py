cf84f9afcc7833a063baac24ebf74da3d1ba8ecd
TevaPhilippe05 <philippe.teva.pro@gmail.com>
2025-02-03 21:03:41 +0100

    Amélioration du tracer des arrêtes afin que ne pas les tracer deux fois si elles se superposent exemple : la laisons a-c et c-a

--- Code/interface.py
+++ Code/interface.py
@@ -6,8 +6,16 @@ from graphe import *
 WIDTH, HEIGHT = 600, 600
 RADIUS_RATIO = 0.4  # Rayon fait 40 % de la largeur de l'écran
 p = 0.4 # Proba de création du graphe (plus elle est élevé, plus il y a d'arrêtes)
-N = 10
+N = 8
 G = Graphe(N)
+
+graphe = [[[540.0, 300.0], 2, 6, 8], [[469.7056274847714, 469.7056274847714], 1, 3, 8], [[300.0, 540.0], 2, 4, 5], 
+          [[130.29437251522862, 469.7056274847714], 3, 8], [[60.0, 300.00000000000006], 3, 7], 
+          [[130.29437251522856, 130.29437251522862], 1, 7, 8], [[299.99999999999994, 60.0], 5, 6], 
+          [[469.70562748477136, 130.29437251522856], 1, 2, 4, 6]]
+
+G.force_graph_coord(graphe)
+
 
 pygame.init()
 screen = pygame.display.set_mode((WIDTH, HEIGHT), pygame.RESIZABLE)
@@ -17,8 +25,6 @@ CENTER = (WIDTH // 2, HEIGHT // 2)
 RADIUS = int(WIDTH * RADIUS_RATIO)
 
 # G.rand_graph_coord(0.2, CENTER, RADIUS)
-graphe = [[[540.0, 300.0], 6, 8], [[469.7056274847714, 469.7056274847714], 8], [[300.0, 540.0], 5], [[130.29437251522862, 469.7056274847714], 8], [[60.0, 300.00000000000006], 3, 7, 8], [[130.29437251522856, 130.29437251522862], 1, 7, 8], [[299.99999999999994, 60.0], 5, 6], [[469.70562748477136, 130.29437251522856], 1, 2, 4, 5, 6]]
-G.force_graph_coord(graphe)
 
 print(G.l_adj)
 
@@ -43,9 +49,12 @@ while running:
         y = G.l_adj[k][0][1]
         pygame.draw.circle(screen, (255, 0, 0), (int(x), int(y)), 8)
 
+
     # Dessiner les liaisons entre les points
-    for l in range(len(G.l_adj)):
+    deja_trace = []
+    for l in range(G.taille):
         for m in range(1, len(G.l_adj[l])):
+            ind_point_1 = l
             x_1 = G.l_adj[l][0][0]
             y_1 = G.l_adj[l][0][1]
             
@@ -53,7 +62,15 @@ while running:
             x_2 = G.l_adj[ind_point_2][0][0]
             y_2 = G.l_adj[ind_point_2][0][1]
 
-            pygame.draw.line(screen, (255,255,255), (x_1, y_1), (x_2, y_2), 5)
+            # Pour ne pas tracer deux fois une ligne
+            deja_trace_b = False
+            for n in range(len(deja_trace)):
+                if deja_trace[n] == [ind_point_2, ind_point_1]:
+                    deja_trace_b = True
+
+            if not deja_trace_b:
+                pygame.draw.line(screen, (255,255,255), (x_1, y_1), (x_2, y_2), 5)
+                deja_trace.append([ind_point_1, ind_point_2])
 
     pygame.display.flip()  # Mise à jour de l'affichage
 
